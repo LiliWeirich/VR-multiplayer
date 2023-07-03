b810cf81487632d6cef6800a5e367e82ac157283
LiliWeirich <lili.weirich@stud.h-da.de>
2023-07-03 15:38:35 +0200

    should work now
    build two times with differen XR rig for player/cave

--- networkplayerSpawner/Assets/Scripts/NetworkPlayerSpawner.cs
+++ networkplayerSpawner/Assets/Scripts/NetworkPlayerSpawner.cs
@@ -5,13 +5,33 @@ using Photon.Pun;
 
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
+    public Transform[] spawnPoints;
+    public GameObject XRLocation;
+
+    private bool doUpdate = false;
     private GameObject _spawnedPlayerPrefab;
+
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
         _spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
         //_spawnedPlayerPrefab.transform.position = XROrigin.transform.position; //need to define XR origin
+        
+        
+        doUpdate = true;
     }
+
+    public void LateUpdate()
+    {
+        if(doUpdate)
+        {
+            int randomSpawn = Random.Range(0, spawnPoints.Length - 1);
+            XRLocation.transform.position = new Vector3(spawnPoints[randomSpawn].position.x, 0.0f, spawnPoints[randomSpawn].position.z);
+            XRLocation.transform.forward = spawnPoints[randomSpawn].forward; // turn so we are looking forward
+            doUpdate = false;
+        }
+    }
+
     public override void OnLeftRoom()
     {
         base.OnLeftRoom();
